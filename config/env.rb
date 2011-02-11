d287b6e5caa41128a365792654b74720cae4d10e
phiggins <pete@peterhiggins.org>
2011-02-10 23:48:11 -0800

    put env name in database file name

--- config/env.rb
+++ config/env.rb
@@ -1,3 +1,5 @@
+RACK_ENV = ENV["RACK_ENV"] || "development"
+
 APP_ROOT = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 
 require 'bundler/setup'
@@ -12,7 +14,7 @@ require 'logger'
 ActiveRecord::Base.logger = Logger.new($stdout)
 ActiveRecord::Base.establish_connection(  
   :adapter  => "sqlite3",  
-  :database => "study_hall.sqlite3")  
+  :database => "study_hall_#{RACK_ENV}.sqlite3")  
 
 lib = File.join(APP_ROOT, "lib")
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
